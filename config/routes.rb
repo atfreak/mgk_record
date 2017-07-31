96607de9ea3f491ab57419848062406c92614698
okano0120 <okano0120@gmail.com>
2017-07-31 12:31:11 +0900

    コントローラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :blog, only: [:index, :show, :list]
+  resources :blog, only: [:index, :show, :list] do
+    collection do
+      get 'list'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
