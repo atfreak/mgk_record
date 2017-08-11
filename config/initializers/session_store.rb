95f62c69d17585f55d52d85be8fe1f73fd1a5038
okano0120 <okano0120@gmail.com>
2017-08-11 15:18:04 +0900

    名前変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_oiwai_rails_session'
+Rails.application.config.session_store :cookie_store, key: '_mgk_record_rails_session'
